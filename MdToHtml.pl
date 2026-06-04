3b08845221c469fa64d3fdc08e48586326b5172c
J. P. Rodrigues <deechtejoao@gmail.com>
2026-06-04 17:45:53 -0300

    MdToHtml.pl: Fix line breaks and blockquote grouping parsing

--- MdToHtml.pl
+++ MdToHtml.pl
@@ -7,67 +7,74 @@ sub parse_markdown {
     my @html;
 
     my $in_list = 0;
+    my @para_lines;
+    my @blockquote_lines;
     my %metadata;
     my $parsing_metadata = 1;
 
     my @lines = split /\n/, $markdown;
 
     for my $line (@lines) {
-        $line =~ s/\s+$//;
-
         # Metadata Parsing
         if ($parsing_metadata) {
-            if ($line =~ /^([a-z]+):\s*(.*)$/i) {
+            my $trimmed = $line;
+            $trimmed =~ s/\s+$//;
+            if ($trimmed =~ /^([a-z]+):\s*(.*)$/i) {
                 $metadata{lc($1)} = $2;
                 next;
-            } elsif ($line eq '') {
+            } elsif ($trimmed eq '') {
                 $parsing_metadata = 0;
                 _render_metadata(\@html, \%metadata) if %metadata;
                 next;
+            } else {
+                $parsing_metadata = 0;
             }
-        } else {
-            $parsing_metadata = 0;
         }
 
         # Headers
         if ($line =~ /^(#{1,6})\s+(.*)$/) {
-            _close_blocks(\@html, \$in_list);
+            _close_active_blocks(\@html, \$in_list, \@para_lines, \@blockquote_lines);
             my $level = length($1);
-            push @html, "<h$level>" . _process_inline($2) . "</h$level>";
+            my $header_text = $2;
+            $header_text =~ s/\s+$//;
+            push @html, "<h$level>" . _process_inline($header_text) . "</h$level>";
         }
         
         # Blockquotes
-        elsif ($line =~ /^>\s+(.*)$/) {
-            _close_blocks(\@html, \$in_list);
-            push @html, "<blockquote>" . _process_inline($1) . "</blockquote>";
+        elsif ($line =~ /^>\s?(.*)$/) {
+            _close_active_blocks(\@html, \$in_list, \@para_lines, []);
+            push @blockquote_lines, $1;
         }
         
         # Unordered Lists (Hyphens or Asterisks)
         elsif ($line =~ /^[-*]\s+(.*)$/) {
+            _close_active_blocks(\@html, \my $dummy, \@para_lines, \@blockquote_lines);
             if (!$in_list) {
                 push @html, "<ul>";
                 $in_list = 1;
             }
-            push @html, "  <li>" . _process_inline($1) . "</li>";
+            my $item_text = $1;
+            $item_text =~ s/\s+$//;
+            push @html, "  <li>" . _process_inline($item_text) . "</li>";
         }
         
         # Blank lines (Structural dividers reset state)
-        elsif ($line eq '') {
-            _close_blocks(\@html, \$in_list);
+        elsif ($line =~ /^\s*$/) {
+            _close_active_blocks(\@html, \$in_list, \@para_lines, \@blockquote_lines);
         }
 
         # Horizontal rules
         elsif ($line =~ /^ {0,3}([-_*])(?:[ \t]*\1){2,}[ \t]*$/) {
-            _close_blocks(\@html, \$in_list);
+            _close_active_blocks(\@html, \$in_list, \@para_lines, \@blockquote_lines);
             push @html, "<hr>";
         }
 
         else {
-            _close_blocks(\@html, \$in_list);
-            push @html, "<p>" . _process_inline($line) . "</p>";
+            _close_active_blocks(\@html, \$in_list, [], \@blockquote_lines);
+            push @para_lines, $line;
         }
     }
-    _close_blocks(\@html, \$in_list);
+    _close_active_blocks(\@html, \$in_list, \@para_lines, \@blockquote_lines);
 
     my $body_content = join("\n", @html);
     my $page_title = $metadata{title} || $filename =~ s/\.[^.]+$/.html/r;
@@ -100,12 +107,72 @@ $body_content
 HTML
 }
 
-sub _close_blocks {
-    my ($html_ref, $in_list_ref) = @_;
+sub _close_active_blocks {
+    my ($html_ref, $in_list_ref, $para_lines_ref, $blockquote_lines_ref) = @_;
     
     if ($$in_list_ref) {
         push @$html_ref, "</ul>";
         $$in_list_ref = 0;
+    }
+    if (@$para_lines_ref) {
+        my @para;
+        for my $l (@$para_lines_ref) {
+            my $has_hard_break = 0;
+            if ($l =~ s/\\$//) {
+                $has_hard_break = 1;
+            } elsif ($l =~ s/ {2,}$//) {
+                $has_hard_break = 1;
+            }
+            $l =~ s/\s+$//;
+            my $processed = _process_inline($l);
+            if ($has_hard_break) {
+                $processed .= "<br>";
+            }
+            push @para, $processed;
+        }
+        push @$html_ref, "<p>" . join("\n", @para) . "</p>";
+        @$para_lines_ref = ();
+    }
+    if (@$blockquote_lines_ref) {
+        my @paragraphs;
+        my @current_p;
+        for my $l (@$blockquote_lines_ref) {
+            my $trimmed_l = $l;
+            $trimmed_l =~ s/\s+$//;
+            if ($trimmed_l eq '') {
+                if (@current_p) {
+                    push @paragraphs, [@current_p];
+                    @current_p = ();
+                }
+            } else {
+                push @current_p, $l;
+            }
+        }
+        if (@current_p) {
+            push @paragraphs, [@current_p];
+        }
+        
+        my @bq_html;
+        for my $p_ref (@paragraphs) {
+            my @para;
+            for my $l (@$p_ref) {
+                my $has_hard_break = 0;
+                if ($l =~ s/\\$//) {
+                    $has_hard_break = 1;
+                } elsif ($l =~ s/ {2,}$//) {
+                    $has_hard_break = 1;
+                }
+                $l =~ s/\s+$//;
+                my $processed = _process_inline($l);
+                if ($has_hard_break) {
+                    $processed .= "<br>";
+                }
+                push @para, $processed;
+            }
+            push @bq_html, "<p>" . join("\n", @para) . "</p>";
+        }
+        push @$html_ref, "<blockquote>\n" . join("\n", @bq_html) . "\n</blockquote>";
+        @$blockquote_lines_ref = ();
     }
 }
 
